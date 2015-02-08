4217f8f612d59ead4e4d5d6025b00f31e4a3cbe2
alass75 <alassaned75@hotmail.fr>
2015-02-08 18:23:00 +0100

    Licence & comment MainActivity

--- Android-development/mobile/src/main/java/com/example/pyrkesa/shwc/MainActivity.java
+++ Android-development/mobile/src/main/java/com/example/pyrkesa/shwc/MainActivity.java
@@ -1,3 +1,28 @@
+/*
+ * MainActivity : This class is used to manage iBeacon service and all fragments of the application.
+ * Moreover the communication with the Android Wear devices is performed on this file thanks to the
+ * Google API.
+ *
+ * Copyright (c) 2015 Pierre-Yves Rancien, Alassane Diagne, Axel Francart, Clément Casasreales, Andreas Roche
+ *
+ * This file is part of SHWC.
+ *
+ * SHWC is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SHWC is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with SHWC. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Contact : projetshwc@gmail.com
+ */
+
 package com.example.pyrkesa.shwc;
 
 import java.util.ArrayList;
