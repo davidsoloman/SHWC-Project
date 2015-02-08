152f496cf2a111c466d8b66d6e113555e675a236
pyrkesa <pierreyvesrancien@gmail.com>
2015-02-08 20:44:00 +0100

    Update BeaconItem.java

--- Android-development/mobile/src/main/java/com/example/pyrkesa/frag/BeaconItem.java
+++ Android-development/mobile/src/main/java/com/example/pyrkesa/frag/BeaconItem.java
@@ -1,3 +1,27 @@
+/*
+ * BeaconItem : This class is used to store and get details about the listview BeaconItem
+ *
+ * Copyright (c) 2015 Pierre-Yves Rancien, Alassane Diagne, Axel Francart, Clément Casasreales, Andreas Roche
+ *
+ * Copyright (c) 2013 Estimote, Inc.
+ *
+ * This file is part of SHWC.
+ *
+ * SHWC is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SHWC is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with SHWC. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Contact : projetshwc@gmail.com
+ */
 package com.example.pyrkesa.frag;
 
 /**
